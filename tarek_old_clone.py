a1b97effca96cd60aa176e7199c3102776740283
SHAH MAKHDUM SHAJON ツ゚ <shahmakhdumshajon@gmail.com>
2025-01-02 16:00:45 +0600

    Create tarek_old_clone.py

---
+++ tarek_old_clone.py
@@ -0,0 +1,16 @@
+#m1
+SHAJON URL : https://b-api.facebook.com/method/auth.login
+-------------------------------------------------------
+SHAJON HEADERS : {'user-agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'x-fb-connection-bandwidth': '26787397', 'x-fb-sim-hni': '24834', 'x-fb-net-hni': '37271', 'x-fb-connection-quality': 'EXCELLENT', 'x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA', 'content-type': 'application/x-www-form-urlencoded', 'x-fb-http-engine': 'Liger', 'Content-Length': '402'}
+-------------------------------------------------------
+SHAJON DATA : {"adid=3007bfd2-50e6-4809-b08b-63c7cda6a573&format=json&device_id=d2a6fe75-8bbd-44df-9386-29cc7bdf8b9e&email=100008411033858&password=8411033858&generate_analytics_claims=1&credentials_type=password&source=login&error_detail_type=button_with_disabled&enroll_misauth=false&generate_session_cookies=1&generate_machine_id=1&meta_inf_fbmeta=&currently_logged_in_userid=0&fb_api_req_friendly_name=authenticate"}
+
+#m2
+
+
+SHAJON URL : https://b-api.facebook.com/auth/login      
+-------------------------------------------------------
+SHAJON HEADERS : {'content-type': 'application/x-www-form-urlencoded', 'Host': 'graph.facebook.com', 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.88 Safari/537.36 Edg/117.0.2045.47', 'accept-encoding': 'gzip, deflate', 'x-fb-http-engine': 'Liger'}
+-------------------------------------------------------
+SHAJON DATA : {'adid': 'd1f33f56-05ef-46a3-bf8e-c5acaaac1602', 'email': '100008357994728', 'password': '123456', 'cpl': 'true', 'credentials_type': 'device_based_login_password', 'source': 'device_based_login', 'error_detail_type': 'button_with_disabled', 'format': 'json', 'generate_session_cookies': '1', 'generate_analytics_claim': '1', 'generate_machine_id': '1', 'family_device_id': '19ac075e-a68e-433e-9795-71833b208e89', 'advertiser_id': '3a0eedc0-4e6a-40f1-8ef3-ca2a19ffe79a', 'locale': 'en_US', 'client_country_code': 'US', 'device_id': '38cb0e68-f542-4f56-9d3c-b8ebf49bf951', 'method': 'auth.login', 'api_key': '882a8490361da98702bf97a021ddc14d', 'fb_api_req_friendly_name': 'authenticate', 'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler'}
+-------------------------------------------------------
